2d9dbec8fa661bc1967c34493a27bdc83ba583b7
Oded Welgreen <oded_wel@hotmail.com>
2012-02-12 21:53:44 +0200

    Change target and add modal view controller

---
+++ OWTapTempoViewControllerDemo/OWTapTempoViewControllerDemo/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  OWTapTempoViewControllerDemo
+//
+//  Created by Oded Welgreen on 12/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
